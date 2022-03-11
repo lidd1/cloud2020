69ba461caa6158a3df17a5028505c0e0661b8d4f
lidiandian <lidiandian@bosssoft.com.cn>
2022-03-11 14:39:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/springcloud/GatewayMain9527.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Lidd
+ * @date 2022-3-9 17:55
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class,args);
+    }
+}
---
+++ src/test/java/T2.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author Lidd
+ * @date 2022-3-10 8:56
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
